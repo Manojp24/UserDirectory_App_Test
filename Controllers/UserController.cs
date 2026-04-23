c134b2ac875289bc64cafff29794922370662c5f
Manojp247 <manojp2487@gmail.com>
2026-04-23 19:12:45 +0530

    InitalTest1

---
+++ Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using UserDirectory_app.EF;
+using UserDirectory_app.Model;
+
+namespace UserDirectory_app.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UserController(AppDbContext context)
+        {
+            appDbContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(await appDbContext.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var user = await appDbContext.Users.FindAsync(id);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(User user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            appDbContext.Users.Add(user);
+            await appDbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = user.Id },user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id,User updated)
+        {
+            if (id != updated.Id) {
+                return BadRequest();
+            }
+
+            appDbContext.Entry(updated).State = EntityState.Modified;
+            await appDbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var user = await appDbContext.Users.FindAsync(id);
+            if (user == null)
+                return NotFound();
+
+            appDbContext.Users.Remove(user);
+            await appDbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+
+using Microsoft.EntityFrameworkCore;
+using UserDirectory_app.EF;
+
+namespace UserDirectory_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+                        opt.UseSqlite("Data Source=app.db"));
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureCreated();
+            }
+            app.UseDeveloperExceptionPage();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+            
+
+            app.Run();
+        }
+    }
+}
